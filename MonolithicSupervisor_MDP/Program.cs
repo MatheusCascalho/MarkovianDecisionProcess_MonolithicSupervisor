495f5363b8a68f793f1f4f68ca695af7e57ea507
MatheusCascalho <45607604+MatheusCascalho@users.noreply.github.com>
2020-04-14 19:03:03 -0300

    redução de mekespan
    na versao anterior, eventos não controláveis simultaneos eram desconsiderados

--- MonolithicSupervisor_MDP/Program.cs
+++ MonolithicSupervisor_MDP/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UltraDES;
-using Politica = System.Collections.Generic.Dictionary<UltraDES.AbstractState, System.Collections.Generic.List<UltraDES.AbstractEvent>>;
+using Politica = System.Collections.Generic.Dictionary<string, System.Collections.Generic.List<string>>;
 using Scheduler = System.Collections.Generic.Dictionary<UltraDES.AbstractEvent, float>;
 using Restriction = System.Collections.Generic.Dictionary<UltraDES.AbstractEvent, uint>;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
 
 namespace MultiAgentMarkovMonolithic
 {
@@ -29,33 +32,106 @@ namespace MultiAgentMarkovMonolithic
 
             // Relacionando cada estado com suas respectivas transições
             allowedEvents = supervisor.Transitions.GroupBy(t => t.Origin).ToDictionary(g => g.Key, g => g.ToArray());
+            var pathPolitica = @"C:\Users\Administrador\Documents\Matheus Cascalho\lacsed\MarkovianDecisionProcess_MonolithicSupervisor\MonolithicSupervisor_MDP\politica.bin";
+
+            ////////////////////////// CRIAR POLITICA E SALVAR EM ARQUIVO .BIN
+            ////////////////////var timerOptimization = new Stopwatch();
+            ////////////////////timerOptimization.Start();
+            ////////////////////Politica map = ValueIterationMethod(supervisor.States, supervisor.Events);
+            ////////////////////timerOptimization.Stop();
+            ////////////////////Console.WriteLine($"\nPolitica de otimizacao encontrada em {timerOptimization.ElapsedMilliseconds / 1000.0} seg");
+            ////////////////////// Guardando a politica em um arquivo .bin
+            ////////////////////try
+            ////////////////////{
+            ////////////////////    FileStream fs = new FileStream(pathPolitica, FileMode.Create);
+            ////////////////////    BinaryFormatter bf = new BinaryFormatter();
+            ////////////////////    bf.Serialize(fs, map);
+            ////////////////////    fs.Close();
+
+            ////////////////////}
+            ////////////////////catch
+            ////////////////////{
+            ////////////////////    Console.WriteLine("Nao Foi possível gerar o arquivo BIN com a politica");
+            ////////////////////}
 
 
-            var timerOptimization = new Stopwatch();
-            timerOptimization.Start();
-            Politica map = ValueIterationMethod(supervisor.States, supervisor.Events);
-            timerOptimization.Stop();
-
-            Console.WriteLine($"\nPolitica de otimizacao encontrada em {timerOptimization.ElapsedMilliseconds / 1000.0} seg");
-
-            for (var i = 0; i < 1000; i += 20)
+            ///LENDO ARQUIVO DA POLITICA
+            Politica map = new Politica();
+            FileInfo fi = new FileInfo(pathPolitica);
+            if (fi.Exists)
             {
-                Console.WriteLine($"{i} pares de produto:\n");
-                List<Transition> transicoes = TransicoesProducao(politica: map, qtdProdutos: i, problema: problema);
-                Console.WriteLine($"Foram realizadads {transicoes.Count} transições\n");
+                Console.WriteLine("Politica encontrada!");
+
+                FileStream fsNovo = new FileStream(pathPolitica, FileMode.Open);
+                BinaryFormatter bfNovo = new BinaryFormatter();
+                map = (Politica)bfNovo.Deserialize(fsNovo);
+                fsNovo.Close();
+
+                Console.WriteLine("Politica lida!!");
+
+                Console.WriteLine("Resultados das simulações:\n");
+                var pathCSV = @"C:\Users\Administrador\Documents\Matheus Cascalho\lacsed\MarkovianDecisionProcess_MonolithicSupervisor\MonolithicSupervisor_MDP\resultado.csv";
+                StreamWriter sw = new StreamWriter(pathCSV, true, Encoding.UTF8);
+
+                var pathTXT = @"C:\Users\Administrador\Documents\Matheus Cascalho\lacsed\MarkovianDecisionProcess_MonolithicSupervisor\MonolithicSupervisor_MDP\eventos.txt";
+                StreamWriter swTXT = new StreamWriter(pathTXT, true, Encoding.UTF8);
+
+
+                //  sw = new StreamWriter(pathCSV, Enco)
+                var cabecalho = "quantidade de pares de produtos;makespan;paralelismo;";
+
+                for (var j = 0; j < 30; j++) // nome das colunas de execucao
+                {
+                    cabecalho += $"execucao {j};";
+                }
+
+                sw.WriteLine(cabecalho);
+                Console.WriteLine(cabecalho);
+                List<int> pares = new List<int>() { 1, 5, 10, 15, 50, 100, 500, 750, 1000 };
+                // for (var i = 0; i < 1000; i += 20) // variando a quantidade de produtos
+                foreach (var i in pares)
+                {
+
+                    Console.WriteLine($"{i} pares de produto:\n");
+
+                    var stringCSV = i.ToString() + ";";
+                    var stringTXT = "";
+                    for (var execucao = 0; execucao < 30; execucao++)
+                    {
+                        var executionTime = new Stopwatch();
+                        executionTime.Start();
+                        SimulationResult simulacao = TransicoesProducao(politica: map, qtdProdutos: i, problema: problema);
+                        executionTime.Stop();
+                        var timeExecution = executionTime.ElapsedMilliseconds / 1000.0;
+
+                        if (execucao == 0)
+                        {
+                            var makespan = simulacao.makespan;
+                            var paralelismo = simulacao.parallelism;
+                            stringCSV += makespan.ToString() + ";" + paralelismo.ToString() + ";";
+                            foreach(var e in simulacao.eventSequence)
+                            {
+                                stringTXT += e + ";";
+                            }
+                            swTXT.WriteLine(stringTXT);
+                        }
+
+                        stringCSV += timeExecution.ToString() + ";";
+                    }
+
+                    sw.WriteLine(stringCSV);
+                    Console.WriteLine(stringCSV);
+                }
+
+                sw.Close();
+                Console.WriteLine("Simulacao finalizada!!!!!!!!");
+
             }
 
-            //var timerProducao = new Stopwatch();
-            //timerProducao.Start();
-            //timerProducao.Stop();
-
-            //Console.WriteLine($"\nProducao realizada em {timerProducao.ElapsedMilliseconds / 1000.0} seg");
-            //foreach (var t in transicoes)
-            //{
-            //    Console.WriteLine(t);
-            //}
-
-
+            else
+            {
+                Console.WriteLine("Politica não encontrada :(");
+            }
         }
 
         /// <summary>
@@ -111,23 +187,27 @@ namespace MultiAgentMarkovMonolithic
         {
             // v = dicionário que relaciona cada estado com sua esperança de ocorrencia
             Dictionary<AbstractState, double> v = new Dictionary<AbstractState, double>();
-
             // mapping = dicionário que relaciona cada estado com uma lista de ações otimas 
-            Politica mapping = new Dictionary<AbstractState, List<AbstractEvent>>();
-
+            Politica mapping = new Politica();
             // d = constante de desconto
-            double d = 0.7;
+            double d = 0.3;
+            // e = Σ_s (v_k[s] - v_{k-1}[s])
+            double erro = 1.0;
+            double precisao = 1e-20;
+            int iteradorVIM = 0;
+
 
             // Atribuindo valores arbitrarios para a v inicial e criando as keys para o mapping
             foreach (var s in stateSet)
             {
                 v.Add(s, 0.0);
-                var n = new List<AbstractEvent>();
-                mapping.Add(s, n);
+                var n = new List<string>();
+                mapping.Add(s.ToString(), n);
             }
 
+
             // Value Iteration Mathod
-            for (var i = 0; i < 100; i++) // RESTRIÇÃO: 100 ITERAÇÕES
+            while ((erro > (precisao * (1 - d)) / (2 * d)) || (iteradorVIM == 1000))            // for (var i = 0; i < 100; i++) // RESTRIÇÃO: 100 ITERAÇÕES
             {
                 Dictionary<AbstractState, double> vAntigo = new Dictionary<AbstractState, double>(v);
                 foreach (var s in stateSet)
@@ -143,7 +223,6 @@ namespace MultiAgentMarkovMonolithic
 
                     // Calculando a esperança de maximização de paralelismo para cada ação
                     List<double> esperancas = new List<double>();
-                    // SortedDictionary<double, AbstractEvent> eventosOrdenadosPorEsperanca = new SortedDictionary<double, AbstractEvent>();
                     List<(double esperanca, AbstractEvent evento)> tuplasEventos = new List<(double esperanca, AbstractEvent evento)>();
                     foreach (var a in eventosPermitidos)
                     {
@@ -153,30 +232,16 @@ namespace MultiAgentMarkovMonolithic
                             sum += (double)Probabilidade(s, sDest, a) * (sDest.ActiveTasks() + d * vAntigo[sDest]);
                         }
                         tuplasEventos.Add((esperanca: sum, evento: a));
-
-                        //if (!eventosOrdenadosPorEsperanca.ContainsKey(sum))
-                        //{
-                        //    eventosOrdenadosPorEsperanca.Add(sum, a);
-                        //}
-                        //else
-                        //{
-                        //    double diff = 1e-10;
-                        //    while (eventosOrdenadosPorEsperanca.ContainsKey(sum - diff))
-                        //    {
-                        //        diff = diff + diff / 10;
-                        //    }
-                        //    eventosOrdenadosPorEsperanca.Add(sum - diff, a); //adiciona uma chave diferente ao dicionário
-                        //}
                         esperancas.Add(sum);
                     }
 
                     v[s] = Max(esperancas);
-                    // v[s] = eventosOrdenadosPorEsperanca.First().Key;
-                    // var eventosOrdenados = eventosOrdenadosPorEsperanca.Values.ToList();
-                    // eventosOrdenados.Reverse();
-                    mapping[s] = tuplasEventos.OrderByDescending(i => i.esperanca).ThenBy(i => i.evento.ToString()).Select(i=> i.evento).ToList();
+                    mapping[s.ToString()] = tuplasEventos.OrderByDescending(i => i.esperanca).ThenBy(i => i.evento.ToString()).Select(i => i.evento.ToString()).ToList();
+
 
                 }
+                erro = Math.Sqrt(v.Zip(vAntigo, (esp, espAntiga) => { return Math.Pow(esp.Value - espAntiga.Value, 2); }).ToList().Sum());
+                iteradorVIM += 1;
             }
 
             return mapping;
@@ -207,69 +272,85 @@ namespace MultiAgentMarkovMonolithic
         /// <param name="qtdProdutos"></param>
         /// <param name="problema"></param>
         /// <returns></returns>
-        static List<Transition> TransicoesProducao(Politica politica, int qtdProdutos, FMS problema)
+        static SimulationResult TransicoesProducao(Politica politica, int qtdProdutos, FMS problema)
         {
             AbstractState estadoAtual = problema.Supervisor.InitialState;
             Restriction restricao = problema.InitialRestriction(qtdProdutos);
             Scheduler scheduler = problema.InitialScheduler();
-            AbstractEvent eventoOtimo = politica[estadoAtual][0];
-            int qtdEventosPossiveis = politica[estadoAtual].Count;
-            int qtdTransicoes = 0;
-            int itEventOtimo = 0;
-            float tempoDeProducao = 0;
-            List<Transition> transicoes = new List<Transition>();
-            bool pararProducao = false;
-
-            // Enquanto existirem eventos permitidos na restricao
-            while (qtdEventosPossiveis > 0)
+            IEnumerable<AbstractEvent> eventSet = problema.Supervisor.Events;
+            string nomeEventoOtimo = politica[estadoAtual.ToString()][0];
+            AbstractEvent eventoOtimo = eventSet.ToArray()[0]; // Assinalamento arbitrario
+            foreach (var e in eventSet)
             {
-                if (restricao.ContainsKey(eventoOtimo))
+                if (e.ToString() == nomeEventoOtimo) {
+                    eventoOtimo = e;
+                    break;
+                }
+            }
+
+
+            int qtdEventosPossiveis = politica[estadoAtual.ToString()].Count;
+            float tempoDeProducao = 0;
+            List<AbstractEvent> sequenciaEventos = new List<AbstractEvent>();
+            uint paralelismoAcumulado = 0;
+            int depth = 44 * qtdProdutos;
+
+
+
+            for (var i = 0; i < depth; i++)
+            {
+                var minimumTime = float.PositiveInfinity;
+                foreach (var e in allowedEvents[estadoAtual])
                 {
-                    if (restricao[eventoOtimo] == 0)
+                    if (!e.Trigger.IsControllable)
                     {
-                        while (restricao[eventoOtimo] == 0)
-                        {
-                            qtdEventosPossiveis -= 1;
-                            itEventOtimo += 1;
-                            if (itEventOtimo < politica[estadoAtual].Count)
-                            {
-                                eventoOtimo = politica[estadoAtual][itEventOtimo];
-                            }
-                            else if ((!float.IsNaN(scheduler[eventoOtimo])) || (itEventOtimo >= politica[estadoAtual].Count))
-                            {
-                                pararProducao = true;
-                                break;
-                            }
-                            if (!restricao.ContainsKey(eventoOtimo)) break;
-                        }
-                        itEventOtimo = 0;
+                        if (scheduler[e.Trigger] < minimumTime) minimumTime = scheduler[e.Trigger];
                     }
                 }
-                if (!(scheduler[eventoOtimo] is float.NaN) && !(scheduler[eventoOtimo] is float.PositiveInfinity)) tempoDeProducao += scheduler[eventoOtimo];
-                if (pararProducao) break;
-                // Atualizando restricao e scheduler
-                restricao = problema.UpdateRestriction(restricao, eventoOtimo);
-                scheduler = problema.UpdatedScheduler(old: scheduler, ev: eventoOtimo);
 
-                // Procurando o proximo estado
-                foreach (var t in allowedEvents[estadoAtual])
+                List<AbstractEvent> forbidenEvents = allowedEvents[estadoAtual].Where(e => restricao.ContainsKey(e.Trigger) ? restricao[e.Trigger] == 0 : ((scheduler[e.Trigger] > minimumTime) || 
+                (float.IsNaN(scheduler[e.Trigger])))).Select(i => i.Trigger).ToList();
+
+                foreach (var e in politica[estadoAtual.ToString()])
                 {
-                    if (t.Trigger == eventoOtimo)
+                    if (!forbidenEvents.Select(i => i.ToString()).Contains(e))
                     {
-                        estadoAtual = t.Destination;
-                        eventoOtimo = politica[estadoAtual][0];
-                        transicoes.Add(t);
+                        foreach (var t in allowedEvents[estadoAtual])
+                        {
+                            if (t.Trigger.ToString() == e)
+                            {
+                                eventoOtimo = t.Trigger;
+                                estadoAtual = t.Destination;
+                                break;
+                            }
+                        }
                         break;
                     }
                 }
-                qtdTransicoes += 1;
-                qtdEventosPossiveis = politica[estadoAtual].Count;
+
+                sequenciaEventos.Add(eventoOtimo);
+                tempoDeProducao += scheduler[eventoOtimo];
+                paralelismoAcumulado += estadoAtual.ActiveTasks();
+                scheduler = problema.UpdatedScheduler(scheduler, eventoOtimo);
+                restricao = problema.UpdateRestriction(restricao, eventoOtimo);
             }
 
-            Console.WriteLine("Já nao existem mais eventos permitidos pela politica");
-            Console.WriteLine($"Tempo de execução: {tempoDeProducao} u.t.\n");
+            var result = new SimulationResult(makespan: tempoDeProducao, parallelism: paralelismoAcumulado, eventSequence: sequenciaEventos);
 
-            return transicoes;
+            return result;
+        }
+
+    }
+
+    public class SimulationResult {
+        public float makespan {get;set;}
+        public uint parallelism { get; set; }
+        public List<AbstractEvent> eventSequence { get; set; }
+        public SimulationResult(float makespan, uint parallelism, List<AbstractEvent> eventSequence)
+        {
+            this.makespan = makespan;
+            this.parallelism = parallelism;
+            this.eventSequence = eventSequence;
         }
     }
 }
